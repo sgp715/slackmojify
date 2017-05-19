024c9ae7751203622af5c769637465725790e577
sgp715 <7sebastianperez@gmail.com>
2017-05-19 13:56:02 -0500

    updating requirements

--- setup.py
+++ setup.py
@@ -7,9 +7,16 @@ setup(
     version="0.1",
     description="Create emojis so that they can be uploaded to Slack.",
     url="https://github.com/sgp715/slackmojify",
+    install_requires=["pillow","imageio"],
     author="Sebastian Perez",
     author_email="7sebastianperez@gmail.com",
     license="MIT",
     keywords=["slack", "emoji"],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Anyone',
+        'License :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
     zip_safe=False
 )
